0ebc9aab8d7b8e7b46f46396e88d983a1fe964b0
Ashni-K <137854616+Ashni-K@users.noreply.github.com>
2024-03-03 17:28:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function check(elm) {
+  // elm now refers to clicked link.
+  elm.href; // you can assign something else now.
+}
